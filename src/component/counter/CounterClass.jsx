66ad26a92911766260fcd76883b5030e6e3e1976
Hamit Mızrak <hamitmizrak@outlook.com>
2024-08-30 22:38:53 +0300

    Counter

---
+++ src/component/counter/CounterClass.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class CounterClass extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CounterClass</Text>
+      </View>
+    )
+  }
+}
---
+++ src/component/counter/CounterFunction.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CounterFunction() {
+  return (
+    <View>
+      <Text>CounterFunction</Text>
+    </View>
+  )
+}
