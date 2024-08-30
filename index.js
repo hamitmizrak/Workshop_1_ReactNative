ebd6f037aa0b3df51f15be4fa2bc10c41bfed129
Hamit Mızrak <hamitmizrak@outlook.com>
2024-08-30 22:38:03 +0300

    index

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// import App from './App';
+
+import {name as appName} from './app.json';
+import Home from './src/component/Home';
+
+// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Home);
